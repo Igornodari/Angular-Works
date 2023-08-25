c8742098f640f6f85b0fdd129ce58cce6015d7ee
Igornodz <igor_lealnodari@hotmail.com>
2023-08-25 11:54:55 -0300

    foi implementado o serviço de produtos e arrumado futuros problemas de vazamento de memoria

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { UserService } from './../../services/user/user.service';
 import { SingupUserRequests } from 'src/app/models/interfaces/user/singupUserRequests';
@@ -7,13 +7,15 @@ import { CookieService } from 'ngx-cookie-service';
 import { MessageService } from 'primeng/api';
 import { AuthGuard } from './../../guards/auth-guard.service';
 import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
   loginCard = true;
   loginForm = this.formBuilder.group({
     email: ['', Validators.required],
@@ -38,35 +40,39 @@ export class HomeComponent implements OnInit {
   }
   onSubmmitLoginForm() {
     if (this.loginForm.valid && this.loginForm.value)
-      this.userService.authUser(this.loginForm.value as AuthRequest).subscribe({
-        next: (response) => {
-          if (response) {
-            this.cookieService.set('USER_INFO', response?.token);
-            this.loginForm.reset();
-            this.router.navigate(['/dashboard']);
+      this.userService
+        .authUser(this.loginForm.value as AuthRequest)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.cookieService.set('USER_INFO', response?.token);
+              this.loginForm.reset();
+              this.router.navigate(['/dashboard']);
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Sucesso !!',
+                detail: `Bem vindo ${response.name} !`,
+                life: 2000,
+              });
+            }
+          },
+          error: (err) => {
             this.messageService.add({
-              severity: 'success',
-              summary: 'Sucesso !!',
-              detail: `Bem vindo ${response.name} !`,
+              severity: 'error',
+              summary: 'Erro',
+              detail: `Erro ao fazer login !`,
               life: 2000,
             });
-          }
-        },
-        error: (err) => {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Erro',
-            detail: `Erro ao fazer login !`,
-            life: 2000,
-          });
-          console.log(err);
-        },
-      });
+            console.log(err);
+          },
+        });
   }
   onSubmmitRegisterForm() {
     if (this.registerForm.value && this.registerForm.valid) {
       this.userService
         .singUpUser(this.registerForm.value as SingupUserRequests)
+        .pipe(takeUntil(this.destroy$))
         .subscribe({
           next: (response) => {
             if (response) {
@@ -89,5 +95,10 @@ export class HomeComponent implements OnInit {
             }),
         });
     }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
